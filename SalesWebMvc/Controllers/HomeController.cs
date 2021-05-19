18579601235775e58dcf2c10318d4aee62b40663
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-05-19 05:25:24 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales WEB MVC App from C# Course.";
+            ViewData["author"] = "Alexandre Rogério";
 
             return View();
         }
